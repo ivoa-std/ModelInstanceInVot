73935862fe68b8b1676a12c9d721b82cd9f80c16
laurentmichel <laurentmichel@localhost>
2021-08-26 11:42:37 +0200

    Utility running test on VOTable.

--- schema/validation/python/utils/test_votable_runner.py
+++ schema/validation/python/utils/test_votable_runner.py
@@ -14,8 +14,6 @@ class TestVOTableRunner:
         
     vodml_validator = Validator(os.path.join(FileUtils.get_schemadir()
                                    , "merged-syntax.xsd"))
-    vodml_validator = Validator("https://raw.githubusercontent.com/ivoa-std/ModelInstanceInVot/master/schema/xsd/merged-syntax.xsd")
-
     @staticmethod
     def testOK(mapping_sample, case_prefix):
         files = os.listdir(mapping_sample)
@@ -24,25 +22,19 @@ class TestVOTableRunner:
         for sample_file in files:
             if sample_file.startswith(ok_prefix) is True:
                 file_path = os.path.join(mapping_sample, sample_file)
+                logger.info("Validate {} against VOTable/v1.3".format(sample_file))
                 if TestVOTableRunner.validator.validate_file(file_path, verbose=False) is False:
                     TestVOTableRunner.validator.validate_file(file_path, verbose=True)
                     logger.error(sample_file + " is not valid, it should be")
                     return  False
+                logger.info("passed")
+                logger.info("Validate {} against merged-syntax.xsd".format(sample_file))
+                if TestVOTableRunner.vodml_validator.validate_file(file_path, verbose=False) is False:
+                    TestVOTableRunner.validator.vodml_validator(file_path, verbose=True)
+                    logger.error(sample_file + " is not valid, it should be")
+                    return  False                
+                logger.info("passed")
                 logger.info(sample_file + " is valid: fine")
-                TestVOTableRunner.vodml_validator.validate_file(file_path, verbose=False)
-                import xml.etree.ElementTree as etree
-                ns = {'dm': 'https://raw.githubusercontent.com/ivoa-std/ModelInstanceInVot/master/schema/xsd/merged-syntax.xsd'}
-                tree = etree.parse(file_path)
-                root = tree.getroot()
-                for actor in root.findall('{http://www.ivoa.net/xml/VOTable/v1.3}RESOURCE'):
-                    print("=====")
-                    for vodml in actor.findall('dm:VODML', ns):
-                        print(etree.tostring(vodml).decode())
-                        TestVOTableRunner.vodml_validator.validate_string(etree.tostring(vodml).decode())
-                xs = xmlschema.XMLSchema11('https://raw.githubusercontent.com/ivoa-std/ModelInstanceInVot/master/schema/xsd/merged-syntax.xsd')
-                #print(xs.is_valid(tree))
-                from pprint import pprint
-                #pprint(xs.to_dict(tree, process_namespaces=False))
         return True
     
     @staticmethod
@@ -54,8 +46,25 @@ class TestVOTableRunner:
         for sample_file in files:
             if sample_file.startswith(ko_prefix) is True:
                 file_path = os.path.join(mapping_sample, sample_file)
+                logger.info("Validate {} against VOTable/v1.3".format(sample_file))
+                passed = 0
                 if TestVOTableRunner.validator.validate_file(file_path , verbose=False) is True:
-                    logger.error(sample_file)
+                    logger.info("passed")
+                    passed += 1
+                else:
+                    logger.info("failed")
+                logger.info("Validate {} against merged-syntax.xsd".format(sample_file))
+                if TestVOTableRunner.vodml_validator.validate_file(file_path , verbose=False) is True:
+                    logger.info("passed")
+                    passed += 1
+               
+                if passed == 2:
+                    logger.info("Both validation passed; at least one shouldn't ")
                     return False
+                else:
+                    logger.info("failed")
+
                 logger.info(sample_file + " is not valid: fine")
+                
+                
         return True
