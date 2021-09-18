054267c7c98d146483c28bc60b0a460de89cf758
mcdittmar <mdittmar@cfa.harvard.edu>
2021-09-18 19:39:07 -0400

    add model and globals tests

---
+++ schema/validation/python/tests/test_12_globals.py
@@ -0,0 +1,24 @@
+'''
+Created on 2021/09/18
+
+@author: laurentmichel
+'''
+import unittest
+from utils.file_utils import FileUtils
+from utils.test_runner import TestRunner
+
+mapping_sample = FileUtils.get_datadir()
+ 
+
+class Test(unittest.TestCase):
+
+    def testOK(self):
+        self.assertTrue(TestRunner.regarde_si_OK(mapping_sample, "test_12"), "file should be valid")
+
+    def testKO(self):
+        self.assertTrue(TestRunner.regarde_si_KO(mapping_sample, "test_12"), "file shouldn't be valid")
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
