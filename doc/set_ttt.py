78239219ad8093f947444c40c63faf93c676fac6
laurentmichel <laurentmichel@localhost>
2022-03-03 17:01:57 +0100

    run a script putting all mapping words in \texttt{..}

--- doc/set_ttt.py
+++ doc/set_ttt.py
@@ -1,21 +1,37 @@
 import re
+import os
+import shutil
 
 keywords = ["ATTRIBUTE", "INSTANCE", "COLLECTION",
             "VODML", "MODEL", "REPORT", "TEMPLATES", "GLOBALS",
-            "REFERENCE", "JOIN", "FOREIGN\\_KEY", "PRIMARY\\_KEY", "WHERE",
+            "REFERENCE", "JOIN", "FOREIGN\\\\_KEY", "PRIMARY\\\\_KEY", "WHERE",
             "@name", "@uri",
             "@dmrole", "@dmtype",
             "@value", "@unit", "@arrayindex",
             "@tableref", "@ref",
             "@dmref", "@dmid", "@url", "@dmid", "@sourceref", "@primarykey", "@foreignkey"
             ]
-# Output 'Jessa_knows_testing_and_machine_learning'
 
-filename = "element_join.tex"
-with open(filename) as fr:
-    str = fr.read()
-    for keyword in keywords:
-        str = re.sub(r"[^\{:](" + keyword + ")[^\}]", ' BaSltexttt{\\1} ', str)
+dirs = os.listdir( "." )
+for filename in dirs:
+    if os.path.isfile(filename)  and filename.endswith(".tex") is True: 
+        print(f"process {filename}")
+        filename_org = filename + ".org"
+        if os.path.exists(filename_org) is False:
+            shutil.copyfile(filename, filename_org
+                        )
+        with open(filename_org) as fr:
+            str = fr.read()
+            for keyword in keywords:
+                str = re.sub(r"[^\{:](" + keyword + ")[^\}]", ' BaSltexttt{\\1} ', str)
+        
+        with   open(filename, 'w') as fw:  
+            fw.write(str.replace('BaSl', '\\').replace(' )', ')'))
     
-    print(str.replace('BaSl', '\\'))
-    
+"""
+To fix by hand
+
+in table missing & or \\
+ e.g. \texttt{@value}  &
+
+"""
